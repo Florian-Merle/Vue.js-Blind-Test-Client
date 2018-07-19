6287eca3d294df24108ba54f0eb88ed404de34ea
florian <florian.david.merle@gmail.com>
2018-07-19 12:57:08 +0200

    Menu added with some style

---
+++ src/components/Menu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menu">
+    <router-link :to="{ path: '/' }">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.menu {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 30px;
+
+  line-height: 30px;
+  margin: 0;
+  padding: 10px;
+
+  background-color: #666;
+  box-shadow: 0 5px 10px #aaa;
+}
+
+.menu a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.menu a.router-link-exact-active {
+  text-decoration: underline;
+}
+</style>
