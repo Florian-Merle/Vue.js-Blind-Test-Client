ae094444655186e863a2db9373dcdd3f32bcf484
florian <florian.david.merle@gmail.com>
2018-07-22 19:07:01 +0200

    Display playlists list

---
+++ src/components/PlaylistListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-link :to="{ path: '/' }">
+    <div class="playlist">
+      <h2>{{ playlist.name }}</h2>
+      <p>{{ playlist.genre }}</p>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ['playlist'],
+};
+</script>
+
+<style scoped>
+.playlist {
+  box-sizing: border-box;
+  width: 50%;
+  float: left;
+  padding: 10px;
+  border: 1px solid #aaa;
+  background-color: #bbb;
+}
+
+a .playlist {
+  color: #000;
+}
+</style>
--- src/components/routerViews/Home.vue
+++ src/components/routerViews/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
-    <h1>Hi</h1>
+    <h1>Choose a playlist</h1>
+    <div class="playlists">
+      <list-item
+        v-for="playlist in playlists"
+        :key="playlist.id"
+        v-bind:playlist="playlist">
+      </list-item>
+    </div>
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag';
+import PlaylistListItem from '@/components/PlaylistListItem';
 
 export default {
   name: 'Home',
+  components: {
+    ListItem: PlaylistListItem,
+  },
   data() {
     return {
       playlists: [],
